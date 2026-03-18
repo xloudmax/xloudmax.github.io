96a1ce94b76c89b6e93addd07b4bca24f3adb18f
xloudmax <xloudmax@gmail.com>
2026-03-18 23:05:02 +0800

    Remove static tags route

---
+++ assets/chunk-4BX2VUAB-OEu10kfv.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./MarkdownViewer-8X5iIC5T.js";function t(c,e){c.accDescr&&e.setAccDescription?.(c.accDescr),c.accTitle&&e.setAccTitle?.(c.accTitle),c.title&&e.setDiagramTitle?.(c.title)}i(t,"populateCommonDb");export{t as p};
